3e8904559f47399ee7e4d04d3aa6d114d57bfd73
Shane <shane.prent.sp@gmail.com>
2025-11-19 00:09:05 +0100

    (Fix): Forgot to add the objects list to the Response DTO

--- FundaStats.Core/Funda/Dtos/FundaResponseDto.cs
+++ FundaStats.Core/Funda/Dtos/FundaResponseDto.cs
@@ -4,4 +4,5 @@ public sealed class FundaResponseDto
 {
     public int TotaalAantalObjecteren { get; set; }
     public required PagingInfo Paging { get; set; }
+    public List<FundaObjectDto> Objects { get; set; } = new();
 }
